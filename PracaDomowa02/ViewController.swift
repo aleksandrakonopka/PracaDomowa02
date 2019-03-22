533f5556608ed24f33e157f78aaa97a3bb9424f5
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-03-22 21:25:26 +0100

    buildings work

--- PracaDomowa02/ViewController.swift
+++ PracaDomowa02/ViewController.swift
@@ -5,16 +5,40 @@
 //  Created by Aleksandra Konopka on 22/03/2019.
 //  Copyright © 2019 Aleksandra Konopka. All rights reserved.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    let buildings = [BuildingView]()
+    
+    func loadBuildings(){
+        var moveBy = 0.0
+        for _ in 0...5{
+            let building = BuildingView.randomBuilding()
+            building.center = CGPoint(x: CGFloat(moveBy) + (building.frame.width/2), y: self.view.bounds.height - (building.frame.height/2))
+            moveBy = moveBy + Double(building.frame.width)
+            scrollView.addSubview(building)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: self.view.bounds.width * 10.0, height: self.view.bounds.height)
+        scrollView.bounds = .zero
+        scrollView.isPagingEnabled = true
+        loadBuildings()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset)
+    }
+    
 }
 
