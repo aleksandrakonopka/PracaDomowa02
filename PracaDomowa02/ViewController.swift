73b457db9e61d1ffcc9fbb528ef5c807c9e77cac
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-03-22 21:05:25 +0100

    Initial Commit

---
+++ PracaDomowa02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracaDomowa02
+//
+//  Created by Aleksandra Konopka on 22/03/2019.
+//  Copyright © 2019 Aleksandra Konopka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
